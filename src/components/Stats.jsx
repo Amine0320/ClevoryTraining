18952dc56170b83b71b06501d603856c091b13c6
Yassine Mhirsi <98595599+Amine0320@users.noreply.github.com>
2023-07-05 19:12:24 +0100

    second commit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,6 @@
 import { stats } from "../constants";
 import styles from "../style";
-
+// STATS SECTION HERE !
 const Stats = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
     {stats.map((stat) => (
